5bf2f5dc7a352a4d81f88fc47c0f408ea7995564
mppavan05 <mppavan05@gmail.com>
2021-12-09 19:45:15 +0530

    Revert "Update Player_controller.cs"
    This reverts commit 55b67f9636bdd62f361dead234c4c304d60003d9.

--- Assets/Player_controller.cs
+++ Assets/Player_controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player_controller : MonoBehaviour
 {
-    public Animator animator;
+    public Animator animator , Animator;
     private void Awake()
     {
         Debug.Log(" player controller awake ");
